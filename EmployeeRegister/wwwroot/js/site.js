b78412da124a22be6083b94b703f57dcedb33dea
Dzmitry Burak <lemif1008@yandex.ru>
2021-11-23 17:02:23 +0300

    небольшие изменения view

--- EmployeeRegister/wwwroot/js/site.js
+++ EmployeeRegister/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function zero_first_format(value)
+{
+    if (value < 10)
+    {
+        value='0'+value;
+    }
+    return value;
+}
 
-// Write your JavaScript code.
+function date_time()
+{
+    let current_datetime = new Date();
+    let day = zero_first_format(current_datetime.getDate());
+    let month = zero_first_format(current_datetime.getMonth()+1);
+    let year = current_datetime.getFullYear();
+    let hours = zero_first_format(current_datetime.getHours());
+    let minutes = zero_first_format(current_datetime.getMinutes());
+    let seconds = zero_first_format(current_datetime.getSeconds());
+
+    return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+}
